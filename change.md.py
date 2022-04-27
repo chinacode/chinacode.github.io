b7eaf19423b4900f6fa2d80bd0dab945d6a72eee
chinacode <chinacode@126.com>
2022-04-27 15:59:47 +0800

    change time

--- change.md.py
+++ change.md.py
@@ -3,8 +3,10 @@ import os
 import re
 import random
 import sys
-
+import time
 from datetime import datetime
+
+post_time_data = {}
 
 
 def write_file(filename, content):
@@ -43,9 +45,84 @@ def is_contain_chinese(strs):
     return False
 
 
+def get_post_time(url):
+    global post_time_data
+    if len(post_time_data) <= 0:
+
+        post_time_list = '''
+gmail-can-not-identify-device	1649770883
+set-windows-default-email	1648659278
+recover-hotmail-email	1648774988
+howto-edm-by-sendinblue	1648702416
+how-to-prevent-gmail-bans	1648829614
+oversea-account-blocked	1648660507
+remove-gmail-account	1648775779
+email-pay-faq	1648659770
+outlook-common-tips	1648658865
+register-a-gmail	1648659233
+import-gmail-contacts-to-outlook	1649863512
+howto-prevent-emails-be-spam	1648660629
+how-to-check-gmail-reg-ip	1649770897
+outlook-hotmail-question	1649687293
+yandex-mail-register-method	1648657277
+gmail-change-password	1648660538
+reg-gmail-without-phone	1648660675
+gmail-account-sale	1648775054
+how-to-find-gmail-email	1648659743
+how-to-keep-google-voice-account	1648658750
+gmail-notice-phone-sms	1646493975
+change-gmail-language	1648660968
+app-go-outsite	1648660569
+amazon-up	1648657820
+gmail-vs-outlook-which-better	1649770856
+top-gmail-extensions-in-2022	1647208344
+top-email-templates-in-2022	1648779608
+introduce-yourself-in-email	1648658237
+gmail-statistics-trend	1649770837
+best-free-bulkemail-providers	1649687264
+outlook-hotmail-account-sale	1648783159
+howto-bulkemail-email-with-gmail	1649770760
+the-best-send-email-time	1648657931
+howto-change-gmail-address	1648574426
+howto-set-gmail-auto-reply	1648659261
+top-youtube-chrome-extensions	1648658606
+useful-temp-email-generators	1648658272
+how-does-gmail-spam-filter-work	1649770813
+create-a-professional-email-address	1648657596
+register-outlook-jump-phone-verify	1648910666
+register-outlook-without-phonenumber	1650720256
+how-to-change-gmail-background	1649761488
+top-google-docs-extensions	1649495375
+how-to-free-us-phone-number	1649687273
+how-to-register-yahoo-email-without-phone-number	1649603139
+how-to-register-google-voice	1649687268
+how-to-register-whatsapp-without-phone	1649690209
+how-to-change-whatsapp-phone-number	1649690659
+iphone-backup-whatsapp-2-icloud	1649777945
+how-to-recover-permanently-deleted-emails-outlook	1649863922
+how-to-recall-sent-email-in-gmail	1649932717
+how-to-transfer-contacts-to-gmail	1650379289
+get-a-free-google-drive-account	1650026352
+listen-whatsapp-audio-without-sender-know	1650030625
+add-whatsapp-friends-without-saving-2-contacts	1650083409
+how-to-save-whatsapp-photos-to-pc	1650084328
+how-to-stop-autoplay-videos-twitter	1650165313
+    '''
+
+        post_times = post_time_list.split("\n")
+        for line in post_times:
+            line = line.strip()
+            if len(line) <= 0:
+                continue
+            url_short, timestamp = list(line.split("\t"))
+            post_time_data[url_short] = time.localtime(int(timestamp))
+
+    return post_time_data[url]
+
+
 if __name__ == '__main__':
     _publish_dir = "./_posts"
-    _post_md_dir = "_site/posts-md"
+    _post_md_dir = "./posts-md"
 
     file_list = os.listdir(_post_md_dir)
     for _f in file_list:
@@ -91,8 +168,13 @@ tags: [{}]
 categories: [{}]  
 ---
 '''
+
         post_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         date_prefix = datetime.now().strftime('%Y-%m-%d')
+        _post_time = get_post_time(url)
+        if _post_time:
+            post_time = time.strftime("%Y-%m-%d %H:%M:%S", _post_time)
+            date_prefix = time.strftime("%Y-%m-%d", _post_time)
 
         generate_md = "{}/{}-{}.md".format(_publish_dir, date_prefix, url)
         header = header.format(title, post_time, url, ','.join(tags), categories)
